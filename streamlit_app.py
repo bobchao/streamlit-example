128c72750d8093824b3b6a2dcf606a2cde258efd
Po-chiang Chao <bobchao@gmail.com>
2023-02-16 16:55:44 +0800

    改！

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,15 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 # nlp = spacy.load("en_core_web_sm")
 
 # 顯示 textarea 用於輸入 OKR
-okr_input = st.text_input("請輸入 Objective：")
+o_input = st.text_input("請輸入 Objective：")
+kr_input = st.text_area("請輸入 KRs，建議每個 KR 一行")
 
 def aihelp(prompt_template):
     # Use OpenAI GPT to analyze the OKR and provide suggestions
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt= prompt_template + ": " + okr_input,
-        max_tokens=1024,
+        prompt= prompt_template + "\n\n Objective: " + o_input + "\nKRs:\n\n" + kr_input,
+        max_tokens=2048,
         n=1,
         stop=None,
         temperature=0.5,
@@ -40,16 +41,16 @@ def aihelp(prompt_template):
 #    st.write("改寫建議")
 #    st.write(suggestion_a)
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
     reviewO = st.button("改善 O")
+    suggestKRs = st.button("建議備選 KR")
+    improveO = st.button("以更積極的態度改寫 O")
 
 with col2:
-    suggestKRs = st.button("建議備選 KR")
-
-with col3:
-    improveO = st.button("以更積極的態度改寫 O")
+    reviewKR = st.button("改善 KR")
+    
 
 
 if reviewO:
@@ -62,4 +63,8 @@ if suggestKRs:
 
 if improveO:
     suggestion_a = aihelp("請將這個 Objective 以更積極的態度提供 5 種改寫版本")
+    st.write(suggestion_a)
+
+if reviewKR:
+    suggestion_a = aihelp("以 OKR 的理論來說，這些 KR 可能有哪些缺點，可能該怎麼改善？（只要列出缺點與改善方式，不要額外解釋或衍生新 KR）")
     st.write(suggestion_a)
